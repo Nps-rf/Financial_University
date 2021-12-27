79ed5f6904f13c90ebbfbec0e7d629381d42bbbd
Nikolai <ya.pikus@gmail.com>
2021-12-27 04:30:59 +0300

    Prepare update v0.1

--- Financial University/Практикум по программированию и АДиСД/Четвертый практикум/Game/Engine/main.py
+++ Financial University/Практикум по программированию и АДиСД/Четвертый практикум/Game/Engine/main.py
@@ -1,3 +1,44 @@
-"""
-Creating structure of program
-"""
+import pygame
+
+
+class Engine:
+    """
+    Abbreviations scheme
+    ◈ First letter -> color of piece
+    ◈ Second letter -> exact piece
+    Example:
+        ◈ wK -> White King
+    Pieces:
+        ◈ K -> King (Король)
+        ◈ Q -> Queen (Ферзь)
+        ◈ R -> Rook (Ладья)
+        ◈ B -> Bishop (Слон)
+        ◈ N -> Knight (Конь)
+        ◈ P -> Pawn (Пешка)
+    """
+    class Main:
+        def __init__(self):
+            self.ratio = {512, 512}
+            self.screen = pygame.display.set_mode
+            self.clock = pygame.time.Clock
+            self.images = Engine.Build.load_images()
+
+        def prepare(self):
+            pygame.init()
+            self.screen = self.screen(self.ratio)
+            self.clock = self.clock()
+            self.screen.fill(pygame.Color('white'))
+            pass
+
+    class Build:
+        @staticmethod
+        def load_images():
+            images = dict()
+            pieces = ['wK', 'wQ', 'wN', 'wR', 'wB', 'wp', 'bK', 'bQ', 'bN', 'bR', 'bB', 'bp']
+            for piece in pieces:
+                images[piece] = pygame.image.load('Pieces/' + piece + '.png')
+            return images
+
+
+if __name__ == '__main__':
+    Chess = Engine()

95e8c1e3ea28abfaf37cfa8500bbf0f25df5db2f
Nikolai <90862688+Nps-rf@users.noreply.github.com>
2021-12-28 06:10:10 +0300

    Update setup.py

--- Financial University/Практикум по программированию и АДиСД/Третий практикум/P4MTD/setup.py
+++ Financial University/Практикум по программированию и АДиСД/Третий практикум/P4MTD/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='P4MTD',
-      version='0.8',
+      version='0.9',
       description='Package For Manipulating Tabular Data',
       packages=['P4MTD'],
       author_email='ya.pikus@gmail.com',

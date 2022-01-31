e7e681646137cd949f80d0907053db7383bdc7d4
Nikolai <ya.pikus@gmail.com>
2022-02-01 02:15:26 +0300

    Andrew and acid

---
+++ YandexInternship/main.py
@@ -0,0 +1,9 @@
+n = int(input())  # 1 <= n <= 100 000 | Amount of tanks
+Volumes = list(map(int, input().split()))  # Volume of each tank
+answer = 0
+maximal = Volumes[0]
+for i in range(len(set(Volumes))):
+    maximal = max(Volumes[i], maximal)
+    if Volumes[i] < maximal:
+        answer = -1
+print(max(Volumes) - min(Volumes) if answer == 0 else answer)
